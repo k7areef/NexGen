eb44292033dbc60e1a671d41ba86d56ebe6da405
k7areef <kh3reef@gmail.com>
2026-02-27 18:38:17 +0200

    feat: Complete About Section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 import SectionLayout from "./common/SectionLayout";
+import reasons from "@data/reasons.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function About() {
     return (
@@ -8,7 +12,23 @@ function About() {
                     title="Reasons to Choose NexGen for Your Digital Journey"
                     headerChildren={<></>}
                 >
-                    Reasons to Choose NexGen for Your Digital Journey
+                    <div className="cards-grid grid grid-cols-1 md:grid-cols-2 gap-5">
+                        {
+                            reasons.map((reason, index) => (<div className="reason-card rounded-lg overflow-hidden bg-dark-10 flex flex-col p-5" key={index}>
+                                <h3 className="uppercase font-medium text-lg sm:text-xl md:text-2xl mb-3">{reason.title}</h3>
+                                <p className="text-grey-70">{reason.description}</p>
+                                <Link
+                                    to={'/'}
+                                    className="flex items-center gap-2 group w-fit mt-5"
+                                >
+                                    <div className="text-orange-80 -rotate-45 w-12 h-12 rounded-full border border-dark-20 flex items-center justify-center shrink-0 transiiton duration-200 ease-in sm:group-hover:border-dark-35">
+                                        <FontAwesomeIcon icon={faArrowRight} />
+                                    </div>
+                                    <p className="text-grey-70 transition duration-200 ease-in sm:group-hover:text-grey-80">Learn More</p>
+                                </Link>
+                            </div>))
+                        }
+                    </div>
                 </SectionLayout>
             </div>
         </section>
