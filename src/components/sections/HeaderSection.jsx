497780bcac7893c8d69553c7bc69037e4db7060d
k7areef <kh3reef@gmail.com>
2026-03-23 11:01:40 +0200

    feat(HeaderSection.jsx): make header section as a dynamic content

--- src/components/sections/HeaderSection.jsx
+++ src/components/sections/HeaderSection.jsx
@@ -1,8 +1,25 @@
-function HeaderSection() {
+/**
+ * @typedef {Object} HeaderSectionProps
+ * @property {React.ReactNode} commonChildren
+ * @property {React.ReactNode} otherChildren
+ */
+
+/**
+ * @param {HeaderSectionProps} props
+ * @returns 
+ */
+function HeaderSection({ commonChildren, otherChildren }) {
     return (
         <section className="header-section-section" id="headerSection">
-            <div className="container">
-                Header Section
+            <div className="container grid grid-cols-1 lg:grid-cols-3 gap-5">
+                {/* Common Content */}
+                <div className="common-content col-span-1 lg:col-span-2">
+                    {commonChildren}
+                </div>
+                {/* Other Content */}
+                <div className="other-content col-span-1">
+                    {otherChildren}
+                </div>
             </div>
         </section>
     )
