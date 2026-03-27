2e117389fe57b731daaef34ae2f90e0fc2c6f158
k7areef <kh3reef@gmail.com>
2026-03-27 07:59:17 +0200

    feat(OurAchievements.jsx): implement dynamic milestones display with structured layout

--- src/components/sections/OurAchievements.jsx
+++ src/components/sections/OurAchievements.jsx
@@ -1,15 +1,34 @@
 import SectionLayout from "./common/SectionLayout";
+import milestones from "../../data/milestones.json";
 
 function OurAchievements() {
     return (
         <section className="our-achievements-section py-5 md:py-10" id="ourAchievements">
-            <div className="container">
+            <div className="container px-4 mx-auto">
                 <SectionLayout
                     title="Our Achievements"
                     className="p-0! border-0!"
                     headerChildren={<></>}
                 >
-                    Our Achievements
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+                        {milestones.map((milestone, index) => (
+                            <div key={index} className="bg-dark-10 border border-dark-15 rounded-xl overflow-hidden flex flex-col h-full">
+                                <div className="heading p-3">
+                                    <span className="text-grey-50 font-medium text-sm md:text-base font-roboto-mono">
+                                        {milestone.date}
+                                    </span>
+                                </div>
+                                <div className="p-3 py-5 bg-dark-12 border-y border-dark-20">
+                                    <h3 className="text-white text-lg md:text-xl font-semibold line-clamp-2">{milestone.title}</h3>
+                                </div>
+                                <div className="p-3">
+                                    <p className="text-grey-50 text-sm md:text-base leading-relaxed">
+                                        {milestone.description}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </SectionLayout>
             </div>
         </section>
